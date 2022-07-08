2cd3d4bdb7796eaac16e986956f9ddf81f1cbdc9
Paula Verónica Quiroga <72896165+RONIE70@users.noreply.github.com>
2022-07-08 02:42:56 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -6,17 +6,18 @@
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online Chatbot Instituto Beltran</title>
-    <link rel="stylesheet" href="style.css">
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script> 
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.js"></script>
-	</head>
+    <link rel="stylesheet" href="style.css">
+</head>
     <style>
     .menu{
     position: relative;
@@ -88,6 +89,7 @@
                     success: function(result){
                         $replay = '<div class="bot-inbox inbox"><div class="icon"><img src="./images/chat.png" ></div><div class="msg-header"><p>'+ result +'</p></div></div>';
                         $(".form").append($replay);
+                        //$(".form").append($server_time);
                         //console.log($replay);
                         // cuando el chat baja, la barra de desplazamiento llega automáticamente al final
                         $(".form").scrollTop($(".form")[0].scrollHeight);
